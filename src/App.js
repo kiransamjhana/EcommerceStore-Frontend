2d3c62de9e3e00daf673b31fe9c921ebcaf0f96c
kiransamjhana <kiranmanandhar956@gmail.com>
2023-09-25 22:25:44 +1000

    signup page starting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
-{
-  /* The following line can be included in your src/index.js or App.js file */
-}
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import { Home } from "./components/home/Home";
 
 import { useEffect } from "react";

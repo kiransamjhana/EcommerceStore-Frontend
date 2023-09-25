d45f408161aa5d596bd51e47bcfd6c54cb70faf6
kiransamjhana <kiranmanandhar956@gmail.com>
2023-09-25 22:34:04 +1000

    sign up page design finish

--- src/components/user/SignUp.js
+++ src/components/user/SignUp.js
@@ -102,7 +102,7 @@ export const SignUp = () => {
             relative z-10"
               >
                 <p class="w-full text-4xl font-medium text-center leading-snug font-serif">
-                  Sign up for an account
+                  Lets Create Your Account today
                 </p>
                 <div class="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
                   <div class="relative">
@@ -110,11 +110,60 @@ export const SignUp = () => {
                       class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
                   absolute"
                     >
-                      Username
+                      First Name
+                    </p>
+                    <input
+                      placeholder="John"
+                      required
+                      type="text"
+                      class="border placeholder-gray-400 focus:outline-none
+                  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+                  border-gray-300 rounded-md"
+                    />
+                  </div>
+                  <div class="relative">
+                    <p
+                      class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
+                  absolute"
+                    >
+                      Last Name
+                    </p>
+                    <input
+                      placeholder="John"
+                      required
+                      type="text"
+                      class="border placeholder-gray-400 focus:outline-none
+                  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+                  border-gray-300 rounded-md"
+                    />
+                  </div>
+                  <div class="relative">
+                    <p
+                      class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
+                  absolute"
+                    >
+                      Home Adress
                     </p>
                     <input
                       placeholder="John"
                       type="text"
+                      required
+                      class="border placeholder-gray-400 focus:outline-none
+                  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+                  border-gray-300 rounded-md"
+                    />
+                  </div>
+                  <div class="relative">
+                    <p
+                      class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
+                  absolute"
+                    >
+                      Phone Number
+                    </p>
+                    <input
+                      placeholder="John"
+                      required
+                      type="Number"
                       class="border placeholder-gray-400 focus:outline-none
                   focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                   border-gray-300 rounded-md"
@@ -126,7 +175,8 @@ export const SignUp = () => {
                     </p>
                     <input
                       placeholder="123@ex.com"
-                      type="text"
+                      required
+                      type="email"
                       class="border placeholder-gray-400 focus:outline-none
                   focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                   border-gray-300 rounded-md"
@@ -140,7 +190,8 @@ export const SignUp = () => {
                       Password
                     </p>
                     <input
-                      placeholder="Password"
+                      placeholder="John"
+                      required
                       type="password"
                       class="border placeholder-gray-400 focus:outline-none
                   focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
@@ -148,12 +199,28 @@ export const SignUp = () => {
                     />
                   </div>
                   <div class="relative">
-                    <a
+                    <p
+                      class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
+                  absolute"
+                    >
+                      Confirm Password
+                    </p>
+                    <input
+                      placeholder="Password"
+                      type="password"
+                      required
+                      class="border placeholder-gray-400 focus:outline-none
+                  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+                  border-gray-300 rounded-md"
+                    />
+                  </div>
+                  <div class="relative">
+                    <Button
                       class="w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-indigo-500
-                  rounded-lg transition duration-200 hover:bg-indigo-600 ease"
+                  rounded-lg transition duration-200 hover:bg-indigo-600 ease "
                     >
                       Submit
-                    </a>
+                    </Button>
                   </div>
                 </div>
               </div>

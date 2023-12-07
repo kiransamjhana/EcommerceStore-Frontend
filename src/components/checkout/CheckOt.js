a68725463f9c4a22a03ac50092c6f7f354581406
kiransamjhana <kiranmanandhar956@gmail.com>
2023-12-07 16:23:20 +0545

    statrt stripe

--- src/components/checkout/CheckOt.js
+++ src/components/checkout/CheckOt.js
@@ -17,86 +17,13 @@ export const CheckOt = () => {
     <div>
       <Header />
       <div className="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
-        <div className="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base">
-          <div className="relative">
-            <ul className="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
-              <li className="flex items-center space-x-3 text-left sm:space-x-4">
-                <a
-                  className="flex h-6 w-6 items-center justify-center rounded-full bg-emerald-200 text-xs font-semibold text-emerald-700"
-                  href="#"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M5 13l4 4L19 7"
-                    />
-                  </svg>
-                </a>
-                <span className="font-semibold text-gray-900">Shop</span>
-              </li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 5l7 7-7 7"
-                />
-              </svg>
-              <li className="flex items-center space-x-3 text-left sm:space-x-4">
-                <a
-                  className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2"
-                  href="#"
-                >
-                  2
-                </a>
-                <span className="font-semibold text-gray-900">Shipping</span>
-              </li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 5l7 7-7 7"
-                />
-              </svg>
-              <li className="flex items-center space-x-3 text-left sm:space-x-4">
-                <a
-                  className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white"
-                  href="#"
-                >
-                  3
-                </a>
-                <span className="font-semibold text-gray-500">Payment</span>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <div className="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base"></div>
       </div>
       <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
         <div className="px-4 pt-8">
           <p className="text-xl font-medium">Order Summary</p>
 
-          <div className="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6">
+          <div className="mt-8 space-y-3 rounded-lg border bg-green px-2 py-4 sm:px-6">
             {cart?.map((item, i) => {
               return (
                 <div
@@ -122,7 +49,7 @@ export const CheckOt = () => {
           </div>
         </div>
         <div className="mt-10 px-4 pt-8 lg:mt-0">
-          <p className="text-xl font-medium">User Details</p>
+          <p className="text-xl font-medium">Delivery information</p>
 
           <div className="">
             <label for="email" className="mt-2 mb-2 block text-sm font-medium">
@@ -184,86 +111,30 @@ export const CheckOt = () => {
                   d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
                 />
               </div>
-            </div>
-            <label
-              for="billing-address"
-              class="mt-4 mb-2 block text-sm font-medium"
-            >
-              Shipping Address
-            </label>
-            <div class="flex flex-col sm:flex-row">
-              <div class="relative flex-shrink-0 sm:w-7/12">
+
+              <label
+                for="billing-address"
+                class="mt-4 mb-2 block text-sm font-medium"
+              >
+                Shipping Address
+              </label>
+
+              <div class="relative ">
                 <input
                   type="text"
-                  id="billing-address"
-                  name="billing-address"
+                  id="shipping-address"
+                  name="shipping-address"
                   class="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                   placeholder="Street Address"
+                  e
                 />
               </div>
-              <select
-                type="text"
-                name="billing-state"
-                class="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-              >
-                State
-                <option value="State">NSW</option>
-                <option value="State">VIC</option>
-                <option value="State">ACT</option>
-                <option value="State">SA</option>
-                <option value="State">QNL</option>
-                <option value="State">WA</option>
-                <option value="State">NT</option>
-              </select>
-              <input
-                type="text"
-                name="billing-zip"
-                class="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/6 focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                placeholder="ZIP"
-              />
             </div>
 
-            <label
-              for="billing-address"
-              class="mt-4 mb-2 block text-sm font-medium"
-            >
-              Billing Adressn
-            </label>
-            <div class="flex flex-col sm:flex-row">
-              <div class="relative flex-shrink-0 sm:w-7/12">
-                <input
-                  type="text"
-                  id="billing-address"
-                  name="billing-address"
-                  class="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                  placeholder="Street Address"
-                />
-              </div>
-              <select
-                type="text"
-                name="billing-state"
-                class="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-              >
-                State
-                <option value="State">NSW</option>
-                <option value="State">VIC</option>
-                <option value="State">ACT</option>
-                <option value="State">SA</option>
-                <option value="State">QNL</option>
-                <option value="State">WA</option>
-                <option value="State">NT</option>
-              </select>
-              <input
-                type="text"
-                name="billing-zip"
-                class="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/6 focus:z-10 focus:border-blue-500 focus:ring-blue-500"
-                placeholder="ZIP"
-              />
-            </div>
             <div class="mt-9 border-t border-b py-2">
               <div class="flex items-center justify-between">
                 <p class="text-sm font-medium text-gray-900">Subtotal</p>
-                <p class="font-semibold text-gray-900">{totalAmount}</p>
+                <p class="font-semibold text-gray-900">${totalAmount}</p>
               </div>
               <div class="flex items-center justify-between">
                 <p class="text-sm font-medium text-gray-900">Shipping</p>
@@ -273,7 +144,7 @@ export const CheckOt = () => {
             <div class="mt-6 flex items-center justify-between">
               <p class="text-sm font-medium text-gray-900">Total to Pay</p>
               <p class="text-2xl font-semibold text-gray-900">
-                {totalAmount - 8}
+                ${totalAmount - 8}
               </p>
             </div>
           </div>
